ccb6289434073af87cb61a979ac2d0dbefe0755f
Carolina <carolina.mesquita983@cs.cruzeirodosul.edu.br>
2025-09-18 10:48:04 -0300

    atualizar

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,9 @@
 import { Component, signal } from '@angular/core';
-import { PrimeiroComponente } from "./primeiro-componente/primeiro-componente";
-import { DataBindings } from "./data-bindings/data-bindings";
+import { Formulario } from './formulario/formulario';
 
 @Component({
   selector: 'app-root',
-  imports: [DataBindings],
+  imports: [Formulario],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
